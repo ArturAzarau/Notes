c947aeb8d71d1b85789e2d63595f4e666ca77f0a
Артур Азаров <arturazarov@macbook-pro-artur.local>
2018-03-09 21:37:40 +0300

    Additional commit

---
+++ Notes(Core Data Example)/View Controllers/Categories/Categories View Controller/CategoriesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesViewController.swift
+//  Notes(Core Data Example)
+//
+//  Created by Артур Азаров on 09.03.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+final class CategoriesViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+}
